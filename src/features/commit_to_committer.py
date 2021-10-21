3448214ac0e3d3a0ca9ff27b403d9bf08dcc8e3e
Emili Bonet i Cervera <emili.bonet@est.fib.upc.edu>
2021-10-21 22:16:15 +0200

    removing more redundant files; adding data modules; moving modelling modules to suitable directory

---
+++ src/features/commit_to_committer.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import time
+
+
+df = pd.read_csv("commit-level_dataframe.csv")
+df_project = df.pivot_table(values=['PROJECT_ID'],
+                            index=['COMMITTER'],
+                            aggfunc={'PROJECT_ID':lambda x: len(x.unique())})
+df_commit_hash = df.pivot_table(values=['COMMIT_HASH'],
+                            index=['COMMITTER'],
+                            aggfunc='count')
+df_mean = df.pivot_table(values=['AVG_LINES_ADDED','AVG_LINES_REMOVED','COUNT_SOLVED','AVG_DURATION','COUNT_REFACTS','FIXED_ISSUES','INDUCED_ISSUES','FILES_CHANGED'],
+                            index=['COMMITTER'],
+                            aggfunc='mean')
+df_committer = pd.merge(pd.merge(df_mean,df_commit_hash,on='COMMITTER'),df_project,on='COMMITTER')
+
+df_committer.reset_index(level=0, inplace=True)
+df_committer.to_csv("committer-level_dataframe.csv", index=False)
